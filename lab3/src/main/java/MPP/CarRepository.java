8c8aae504fe2e718298f4df335e52759445b768c
B-A-Programs <tudoracheandrei09@gmail.com>
2025-03-11 17:30:48 +0200

    Tema lab 3

---
+++ lab3/src/main/java/MPP/CarRepository.java
@@ -0,0 +1,8 @@
+package MPP;
+
+import java.util.List;
+
+public interface CarRepository extends Repository<Integer,Car> {
+    List<Car> findByManufacturer(String manufacturer);
+    List<Car> findBetweenYears(int min, int max);
+}
---
+++ lab3/src/main/java/MPP/Identifiable.java
@@ -0,0 +1,6 @@
+package MPP;
+
+public interface Identifiable<ID> {
+    void setId(ID id);
+    ID getId();
+}
